73b21e78d4ea925ac0241a859d354e2bbdf75768
Robert Jimenez <robertjimenez86@gmail.com>
2022-06-05 16:19:10 -0500

    added signup capability to frontend, signing up creates a new user in db

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,6 +45,37 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// login
+router.post("/login", (req, res) => {
+  console.log(req.body.email, req.body.password);
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    // req.session.save(() => {
+    //   req.session.user_id = dbUserData.id;
+    //   req.session.username = dbUserData.username;
+    //   req.session.loggedIn = true;
+
+    // });
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
+
 // create a new user
 router.post("/", (req, res) => {
   User.create({
--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 class User extends Model {
-  //   checkPassword(loginPw) {
-  //     return bcrypt.compareSync(loginPw, this.password);
-  //   }
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 User.init(
