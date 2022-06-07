c4ae414823c91e0343acb8c7a6bb9de315fc258a
Robert Jimenez <robertjimenez86@gmail.com>
2022-06-06 22:37:17 -0500

    added nodemailer and sent first email

--- server.js
+++ server.js
@@ -30,7 +30,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "/public/images")));
-app.use(require('./controllers/'));
+app.use(require("./controllers/"));
+
+var nodemailer = require("nodemailer");
+const { getMaxListeners } = require("process");
+var transporter = nodemailer.createTransport({
+  host: "smtp-relay.sendinblue.com",
+  port: 587,
+  auth: {
+    user: "rjimeneztech@gmail.com",
+    pass: process.env.DB_AOL_PW,
+  },
+});
+
+var mailOptions = {
+  from: "habitual2022@aol.com",
+  to: "rjimeneztech@gmail.com",
+  subject: "Sending Email using Nodemailer and Node.js",
+  text: "This is the email!!!",
+};
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
