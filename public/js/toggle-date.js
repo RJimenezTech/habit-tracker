f613aac66f3075aa1c57ea5ce5ae845b4632e9b3
Robert Jimenez <robertjimenez86@gmail.com>
2022-06-08 02:20:39 -0500

    updated toggle-date function

--- public/js/toggle-date.js
+++ public/js/toggle-date.js
@@ -3,49 +3,39 @@ async function toggleDateHandler(event) {
 
   // const description = document.querySelector('div[id="date-box"]').value.trim();
 
-  const habit_id = event.target.getAttribute("data-habit-id");
-  const date = event.target.getAttribute("data-date");
+  const habit_id = parseInt(event.target.getAttribute("data-habit-id"));
+  const date = parseInt(event.target.getAttribute("data-date-id"));
 
   console.log(date, habit_id);
 
-  // check if the date gets a response, a good response means that
-  // a habit was done on that date and is recorded in the db
-  const check_response = await fetch(`/api/dates/${date}/${habit_id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  if (event.target.classList.contains("has-background-danger")) {
+    const response = await fetch(`/api/dates/`, {
+      method: "POST",
+      body: JSON.stringify({
+        date: date,
+        habit_id: habit_id,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  console.log(check_response);
+    console.log(await response);
+  }
 
-  const response = await fetch(`/api/dates/`, {
-    method: "POST",
-    body: JSON.stringify({
-      date: date,
-      habit_id: habit_id,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  console.log(response);
-  //   if (!response.ok) {
-  //     alert("date updated!");
-  //   } else {
-  //     const response = await fetch(`/api/dates/`, {
-  //       method: "DELETE",
-  //       body: JSON.stringify({
-  //         date: date,
-  //         habit_id: habit_id,
-  //       }),
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //     alert("date removed!");
-  //   }
+  if (event.target.classList.contains("has-background-grey-light")) {
+    const response = await fetch(`/api/dates/`, {
+      method: "DELETE",
+      body: JSON.stringify({
+        date: date,
+        habit_id: habit_id,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(await response);
+  }
 }
 
 document.querySelectorAll(".date-box").forEach((box) => {
